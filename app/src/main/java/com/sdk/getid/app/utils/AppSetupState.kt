d370c845beba70ecb73461e51e1ebf659e6769d1
nazarkorchak <nazaryikorchak@gmail.com>
2021-01-29 15:58:26 +0200

    - update documentation

--- app/src/main/java/com/sdk/getid/app/utils/AppSetupState.kt
+++ app/src/main/java/com/sdk/getid/app/utils/AppSetupState.kt
@@ -2,7 +2,7 @@ package com.sdk.getid.app.utils
 
 class AppSetupState {
     companion object{
-        val GET_ID_SDK_DOMAIN = "https://gerus.dev.getid.dev/"
-        val GET_ID_API_KEY = "1231223"
+        val GET_ID_SDK_DOMAIN = "API_URL"
+        val GET_ID_API_KEY = "SDK_KEY"
     }
 }
